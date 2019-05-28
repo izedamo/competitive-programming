bf9dae9d6a54434265d84d33b11647cff04855de
Shubham Bassi <batman@BatComputer.local>
2019-05-29 01:05:30 +0530

    Completed.

--- codechef/JOHNY.py
+++ codechef/JOHNY.py
@@ -1,6 +1,13 @@
+#https://www.codechef.com/problems/JOHNY
+
 t = int(input().strip())
 
 for _ in range(t):
     n = int(input().strip())
     songs = [int(__) for __ in input().strip().split()]
-    k = int(input().strip())
+    k = int(input().strip()) - 1
+    j_song = songs[k]
+    position = 1
+    for song in songs:
+        if song < j_song: position += 1
+    print(position)
